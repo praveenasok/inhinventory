aa08baf6930ebc4a890c8a911aae6df18c90558a
Praveen Asok <praveenasok@Praveens-Mac-mini.local>
2026-08-28 15:05:32 +0530

    chore: remove firebase local emulator for dev mode to restore access to live database

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   projectId: "inhsuite",
@@ -15,15 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-
-// Use emulator if in development
-if (import.meta.env.DEV) {
-  try {
-    connectFirestoreEmulator(db, 'localhost', 8080);
-    connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-  } catch (e) {
-    console.error("Emulator connection failed", e);
-  }
-}
 
 export { app, db, auth };
